c932e099ae5a9a4f05b0035655b88e2b48023c92
Praveen <praveenbailur03@gamail.com>
2026-04-07 19:47:37 +0530

    Random emoji added

---
+++ 21. Random Emojis/app.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = [
+  "😀",
+  "😃",
+  "😄",
+  "😁",
+  "😆",
+  "😅",
+  "😂",
+  "🙂",
+  "🙃",
+  "😉",
+  "😊",
+  "😇",
+  "🥰",
+  "😍",
+  "🤩",
+  "😘",
+  "😗",
+  "😚",
+  "😙",
+  "😋",
+  "😜",
+  "😝",
+  "😛",
+  "🤑",
+  "🤗",
+  "🤭",
+  "🤫",
+  "🤔",
+  "🤐",
+  "🤨",
+  "😐",
+  "😑",
+  "😶",
+  "😏",
+  "😒",
+  "🙄",
+  "😬",
+  "😮‍💨",
+  "😌",
+  "😔",
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
