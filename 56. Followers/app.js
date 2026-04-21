ba8d15be961e5627194008d85c4ee7a652eeeccc
Praveen <praveenbailur03@gamail.com>
2026-04-21 09:42:12 +0530

    Followers

---
+++ 56. Followers/app.js
@@ -0,0 +1,21 @@
+const followers = document.querySelectorAll(".followers");
+
+followers.forEach((followersCounter) => {
+  followersCounter.innerText = "0";
+
+  const updateFollowersCounter = () => {
+    const target = +followersCounter.getAttribute("data-target");
+    const c = +followersCounter.innerText;
+
+    const increment = target / 500;
+
+    if (c < target) {
+      followersCounter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateFollowersCounter, 1);
+    } else {
+      followersCounter.innerText = target;
+    }
+  };
+
+  updateFollowersCounter();
+});
