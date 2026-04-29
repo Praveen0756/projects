d6f8489a2e746f87c38252a3d985c158b0c3b42c
Praveen <praveenbailur03@gamail.com>
2026-04-29 09:04:19 +0530

    Bulb Project

---
+++ 73. Light Bulb/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn");
+let body = document.body;
+let audio = document.querySelector("#audio");
+
+btn.addEventListener("click", () => {
+  body.classList.toggle("on");
+  audio.play();
+});
